8cd27070bc631e4aba1ce040690ffc947f163ae9
Linyu Dong <daisydon@brandeis.edu>
2014-05-23 15:12:52 -0700

    8puzzles

--- HW4/src/Board.java
+++ HW4/src/Board.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -5,22 +6,34 @@ import java.util.Arrays;
  */
 
 public class Board {
-    private int[][] board;
+    private short[][] board;
     private final int N;
-    private int[][] goal;   //how to make it final
+
+    private int cachedMahattan;
+
 
     //construct a board from an N-by-N array of blocks
     public Board(int[][] blocks) {
             N = blocks.length;
-            int counter = 0;
+            board = new short[N][N];
+
+
             for (int i = 0; i < N; i++) {
-                counter++;
                 for (int j = 0; j < N; j++) {
-                    board[i][j] = blocks[i][j];
-                    goal[i][j] = counter;
+                    board[i][j] = (short) blocks[i][j];
                 }
             }
-        goal[N - 1][N - 1] = 0;
+    }
+
+    private Board(short[][] blocks) {
+        N = blocks.length;
+        board = new short[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = blocks[i][j];
+            }
+        }
     }
 
     //board dimension N
@@ -28,17 +41,16 @@ public class Board {
         return N;
     }
 
+
     //number of blocks out of space
     public int hamming() {
-        int counter = 0;
+        int ret = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (i == N - 1 && j == N - 1) continue;
-
-                else if (board[i][j] != goal[i][j]) counter++;
+                if (board[i][j] != 0 && board[i][j] != i * N + j + 1) ret++;
             }
         }
-        return counter;
+        return ret;
     }
 
     public int manhattan()                 // sum of Manhattan distances between blocks and goal
@@ -47,7 +59,7 @@ public class Board {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (board[i][j] == 0) continue;
-                else if (board[i][j] != goal[i][j]) {
+                else if (board[i][j] != i * N + j + 1) {
                     int x = board[i][j] / N;
                     int y = board[i][j] % N;
                     if (y > 0) distance += Math.abs(x - i) + Math.abs(y - 1 - j);
@@ -55,6 +67,8 @@ public class Board {
                 }
             }
         }
+        //Optimization
+        cachedMahattan = distance;
         return distance;
     }
 
@@ -75,18 +89,23 @@ public class Board {
 
     public Board twin()                    // a board obtained by exchanging two adjacent blocks in the same row
     {
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (board[i][j] != 0 && j + 1 < N && board[i][j + 1] != 0) {
-                    int tmp = board[i][j];
-                    board[i][j] = tmp;
-                    board[i][j] = board[i][j + 1];
-                    board[i][j + 1] = tmp;
+                    Board twin = new Board(board);
+                    swapInRow(twin.board, new Dimension(i, j), new Dimension(i, j+1));
+                    return twin;
                 }
             }
         }
-        Board ret = new Board(board);
-        return ret;
+        throw new IllegalArgumentException();
+    }
+
+    private void swapInRow(short[][] b, Dimension d1, Dimension d2)
+    {
+        short tmp = b[d1.x][d1.y];
+        b[d1.x][d1.y] = b[d2.x][d2.y];
+        b[d2.x][d2.y] = tmp;
     }
 
 
@@ -99,28 +118,99 @@ public class Board {
         return (Arrays.deepEquals(this.board, that.board) && this.N == that.N);
     }
 
-    public Iterable<Board> neighbors()     // all neighboring boards
-    {
-        Stack<Board> tmp = new Stack<Board>();
+    private class Dimension {
+        private int x;
+        private int y;
+        public Dimension(int m, int n) {
+            this.x = m;
+            this.y = n;
+        }
+        public Dimension setX(int m) {
+             this.x = m;
+             return this;
+        }
+        public Dimension setY(int n) {
+            this.y = n;
+            return this;
+        }
 
-
-        return tmp;
     }
 
+    private Dimension find(short[][] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == value) {
+                    return new Dimension(i, j);
+                }
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 
+    public Iterable<Board> neighbors()     // all neighboring boards
+    {
+        Dimension zero = find(board, 0);
+        ArrayList<Board> ret = new ArrayList<Board>();
+        if (zero.x >= 0 && zero.y >= 0) {
+            //up
+            if (zero.y - 1 >= 0) {
+                Board tmp = new Board(board);
+                swapWithZero(tmp.board, zero, new Dimension(zero.x, zero.y - 1));
+                ret.add(tmp);
+            }
+            //down
+            if (zero.y + 1 < N) {
+                Board tmp = new Board(board);
+                swapWithZero(tmp.board, zero, new Dimension(zero.x, zero.y + 1));
+                ret.add(tmp);
+            }
+            //left
+            if (zero.x - 1 >= 0) {
+                Board tmp = new Board(board);
+                swapWithZero(tmp.board, zero, new Dimension(zero.x - 1, zero.y));
+                ret.add(tmp);
+            }
+            //right
+            if (zero.x + 1 < board[0].length) {
+                Board tmp = new Board(board);
+                swapWithZero(tmp.board, zero, new Dimension(zero.x + 1, zero.y));
+                ret.add(tmp);
+            }
+            return ret;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void swapWithZero(short[][] b, Dimension zero, Dimension d) {
+        b[zero.x][zero.y] = b[d.x][d.y];
+        b[d.x][d.y] = 0;
+    }
 
 
     public String toString()               // string representation of the board (in the output format specified below)
     {
         StringBuilder sb = new StringBuilder();
         sb.append(N);
+        sb.append('\n');
         for (int i = 0; i < N; i++) {
-            sb.append("/n");
             for (int j = 0; j < N; j++) {
-                sb.append(String.format("%2d",board[i][j]));
+                sb.append(String.format(String.format("%%%dd", 1 + digits(N * N - 1)), board[i][j]));
             }
+            sb.append('\n');
         }
         return sb.toString();
+    }
+
+    private static int digits(int k)
+    {
+        int nd = 1;
+        int remain = k;
+
+        while (remain >= 10) {
+            nd++;
+            remain /= 10;
+        }
+        return nd;
     }
 
 }
--- HW4/src/Solver.java
+++ HW4/src/Solver.java
@@ -1,53 +1,154 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-/**
- * Created by linyu on 3/27/14.
- */
 
 public class Solver {
-    private Board initState;
-    private List<Board> Solution;
+
+    private final List<Board> solution;
 
 
     public Solver(Board initial)            // find a solution to the initial board (using the A* algorithm)
     {
-        this.Solution = solve(initial);
-
+        solution = solve(initial);
     }
 
     public boolean isSolvable()             // is the initial board solvable?
     {
-        return Solution.size() != 0;
+        return solution != null;
     }
 
     public int moves()                      // min number of moves to solve initial board; -1 if no solution
     {
-        if (Solution.size() == 0) return -1;
-        return Solution.size() - 1;
+        if (solution == null) return -1;
+        else
+            return solution.size() - 1;
     }
 
     public Iterable<Board> solution()       // sequence of boards in a shortest solution; null if no solution
     {
-        return Solution;
+        return solution;
     }
 
-    private List<Board> solve(Board initial) {
-        List<Board> ret = new LinkedList<Board>();
+    private List<Board> solve(Board init) {
+        MinPQ<Node> origin = new MinPQ<Node>(1, Node.MANHATTAN);
+        MinPQ<Node> twin = new MinPQ<Node>(1, Node.MANHATTAN);
 
-        return ret;
+        origin.insert(new Node(init, null, 0));
+        twin.insert(new Node(init.twin(), null, 0));
+
+        List<SolutionInfo> candidates = Arrays.asList(new TrueSolution(origin), new VirtualSolution(twin));
+
+        try {
+            while (noneTerminate(candidates)) {
+                for (SolutionInfo candidate : candidates) {
+                    candidate.bfs();
+                }
+            }
+        }
+        catch (SolutionFoundException e) {
+            return e.solution;
+        }
+        throw new IllegalStateException();
     }
 
-    private class Node {
-        private Board board;
-        private Node pre;
-        private int moves;
+    private boolean noneTerminate(List<SolutionInfo> candidates) {
+        for (SolutionInfo candidate: candidates) {
+            if (candidate.isTerminal()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        public final Comparator<Node> MANHATTAN = new Comparator<Node>() {
+    private final class TrueSolution extends SolutionInfo {
+
+        public TrueSolution(MinPQ<Node> queue) {
+            super(queue);
+        }
+
+        @Override
+        public List<Board> getPath(Node goal) {
+            List<Board> list = new ArrayList<Board>();
+
+            Node cur = goal;
+            while (cur != null) {
+                list.add(cur.board);
+                cur = cur.pre;
+            }
+            Collections.reverse(list);
+            return list;
+        }
+    }
+
+    private final class VirtualSolution extends SolutionInfo {
+
+        public VirtualSolution(MinPQ<Node> queue) {
+            super(queue);
+        }
+
+        @Override
+        public List<Board> getPath(Node goal) {
+            return null;
+        }
+    }
+    private abstract class SolutionInfo {
+        private final MinPQ<Node> queue;
+
+        public SolutionInfo(MinPQ<Node> q) {
+            this.queue = q;
+        }
+
+        public boolean isTerminal() {
+            return queue.isEmpty();
+        }
+
+        public void bfs() throws SolutionFoundException {
+            Node min = queue.delMin();
+            if (min.board.isGoal()) {
+                throw new SolutionFoundException(getPath(min));
+            } else {
+                for (Board b: min.board.neighbors()) {
+                    if (min.pre == null || !min.pre.board.equals(b)) {
+                        queue.insert(new Node(b, min, min.moves + 1));
+                    }
+                }
+            }
+        }
+
+        public abstract List<Board> getPath(Node goal);
+    }
+
+
+    private final class SolutionFoundException extends Exception {
+        private final List<Board> solution;
+
+        public SolutionFoundException(List<Board> solution) {
+            this.solution = solution;
+        }
+
+        public List<Board> getSolution() {
+            if (solution == null) return null;
+            else return Collections.unmodifiableList(solution);
+        }
+    }
+
+    private static final class Node {
+
+        public static final Comparator<Node> MANHATTAN = new Comparator<Node>() {
             @Override
             public int compare(Node node1, Node node2) {
                 return (node1.board.manhattan() + node1.moves) - (node2.board.manhattan() + node2.moves);
             }
         };
+
+        private Board board;
+        private Node pre;
+        private int moves;
+
+
 
         public Node(Board b, Node p, int m) {
             this.board = b;
@@ -55,20 +156,16 @@ public class Solver {
             this.moves = m;
         }
 
-        public void setMoves(int m) {
-            this.moves = m;
+        public Board getBoard() {
+            return board;
         }
 
-        public Node pre() {
-            return this.pre;
+        public Node getPre() {
+            return pre;
         }
 
-        public int steps() {
-            return this.moves;
-        }
-
-        public Board board() {
-            return this.board;
+        public int getMoves() {
+            return moves;
         }
     }
 
